5f76a0e6213d4c59578cbbb1a581a7c8f06e6b68
rm18adc <rm18adc@ENT-D410-35.herts.ac.uk>
2019-04-08 13:25:15 +0100

    created selected start back button

--- src/embeddedmediaplayer/MediaControl.java
+++ src/embeddedmediaplayer/MediaControl.java
@@ -345,6 +345,12 @@ public class MediaControl extends VBox {
         btnNudgeSelectedStartBack.setStyle("-fx-max-width:infinity");
         btnNudgeSelectedStartBack.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent e) {
+             Clip p = table.getSelectionModel().getSelectedItem();
+                if (p==null) return;
+                int CurrentPosition = p.getEnd();
+                int ReducedOneSecond = CurrentPosition - 1;
+                p.setEnd(ReducedOneSecond);
+                doTableRefresh(table);   
             }
         });
         final Button btnNudgeSelectedStartForward = new Button(">>");
@@ -510,7 +516,7 @@ public class MediaControl extends VBox {
           //gridBox.add(btnNudgeBack,6,1,1,1);
           //gridBox.add(btnNudgeForward,6,1,1,1);
           gridBox.add(btnAllCapsSelected,7,1,1,1);
-          //gridBox.add(btnNudgeSelectedStartBack,8,1,1,1);
+          gridBox.add(btnNudgeSelectedStartBack,8,1,1,1);
           //gridBox.add(btnNudgeSelectedStartForward,8,1,1,1);
           
           
