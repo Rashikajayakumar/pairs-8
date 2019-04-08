a4d000f963f555f2a6f694a7ea401869392177e1
rm18adc <rm18adc@ENT-D410-35.herts.ac.uk>
2019-04-08 13:36:21 +0100

    created empty string to keep previous value

---
+++ test/embeddedmediaplayer/ClipTest.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package embeddedmediaplayer;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author comqsjb
+ */
+public class ClipTest {
+    
+    public ClipTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getTitle method, of class Clip.
+     */
+    @Test
+    public void testSetTitleToEmptyStringKeepsPreviousValue()
+    {
+        System.out.println("setTitle");
+         Clip instance = new Clip();
+         String OriginalTitle = "Original Title";
+        instance.setTitle(OriginalTitle);              
+        String EmptyTitle = " ";
+        instance.setTitle(EmptyTitle);              // try to set Empty Title
+        String ModifiedTitle = instance.getTitle(); // check Empty Title is set or not 
+         assertTrue(OriginalTitle.equals(ModifiedTitle));// compare original title and resulted title
+         System.out.println("Empty Title is not set");
+    }
+
+    @Test
+    public void testSetEndBeforeStartKeepsPreviousValue()
+    {
+    }
+
+    @Test
+    public void testEqualsOnEqualClips() 
+    {
+    }
+    
+    @Test
+    public void testEqualsOnNonEqualClips() 
+    {
+    }
+    
+    @Test
+    public void testSetEndToNegativeNumberKeepsPreviousValue() 
+    {
+    }
+    
+    @Test
+    public void testSetStartToValidPositiveNumber() 
+    {    
+    }
+    
+    
+    
+}
